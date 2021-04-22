e6d5be68a6f507b62f7a6c5270d9964dfcbd1e0e
Szczepan Masny <szczepanmasny@gmail.com>
2021-04-22 18:08:16 +0200

    feat: add stories for icon button

---
+++ src/docs/views/Docs/Components/IconButton/Component.vue
@@ -0,0 +1,36 @@
+<template>
+  <div :class="$bem({})">
+    <PageHeader>
+      Icon button
+    </PageHeader>
+    <IconButtonExample />
+    <ComponentApi :component="component" />
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { VIconButton } from '@/components';
+import IconButtonExample from './Example.vue';
+import {
+  PageHeader,
+  ComponentApi
+} from '@/docs/components';
+
+export default defineComponent({
+  name: 'DocsComponentsIconButton',
+  components: {
+    PageHeader,
+    ComponentApi,
+    IconButtonExample
+  },
+  data () {
+    return {
+      component: VIconButton
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+</style>
