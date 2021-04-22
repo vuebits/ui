e756f742e55ab9f82791b0a097407e54db8ec70d
Szczepan Masny <szczepanmasny@gmail.com>
2021-04-22 21:28:44 +0200

    redesign home page

--- src/docs/views/Home/Component.vue
+++ src/docs/views/Home/Component.vue
@@ -1,42 +1,45 @@
 <template>
   <div :class="$bem({})">
     <div :class="$bem({e: 'content'})">
-      <img src="@/assets/images/logo.png">
+      <img
+        src="@/assets/images/logo.png"
+        :class="$bem({e: 'logo'})"
+      >
       <h1 :class="$bem({e: 'title'})">
         Vuebits UI
       </h1>
-      <h2 :class="$bem({e: 'subtitle'})">
+      <div :class="$bem({e: 'subtitle'})">
         Vue 3 components library
-      </h2>
+      </div>
       <VSimpleGrid :elements-md="3">
         <VSimpleGridItem>
           <VButton
+            dark
             color="secondary"
-            round
-            elevated
+            rounded
             :class="$bem({e: 'button'})"
             :to="docsRoute"
           >
             <VIcon
               name="book-open"
-              size="lg"
+              class="has-margin-right-sm"
             />
             Get started
           </VButton>
         </VSimpleGridItem>
         <VSimpleGridItem>
           <VButton
-            dark
-            color="dark"
-            round
-            elevated
+            bordered
+            hoverable
+            rounded
             :class="$bem({e: 'button'})"
             href="https://github.com/vuebits/ui"
             new-window
           >
             <VIcon
               name="github"
-              size="lg"
+              type="fab"
+              class="has-margin-right-sm"
             />
             GitHub
           </VButton>
@@ -44,16 +47,16 @@
         <VSimpleGridItem>
           <VButton
             dark
-            color="error"
-            round
-            elevated
+            color="primary"
+            rounded
             href="https://www.npmjs.com/package/@vuebits/ui"
             new-window
             :class="$bem({e: 'button'})"
           >
             <VIcon
               name="npm"
-              size="lg"
+              type="fab"
+              class="has-margin-right-sm"
             />
             npm
           </VButton>
